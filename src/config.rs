42eb9c12f8d3e49268beddb945eee5ec9751d4f1
David <32793517+dvdhellin@users.noreply.github.com>
2026-03-20 10:35:40 +0100

    Añadidos mis servidores

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["remoto.redsion.es"];
+pub const RS_PUB_KEY: &str = "Uf0OiPBLuSDS4cbNVaHCGDun8EH+wmnG0yIF1sM9hQU=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
